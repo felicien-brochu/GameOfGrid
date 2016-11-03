ba7643a9ae992d532e698f2f89bd65b26dcc85c8
Félicien Brochu <felicien.brochu@gmail.com>
2016-11-03 20:00:52 +0100

    Tutorial.

--- js/src/i18n.js
+++ js/src/i18n.js
@@ -3,12 +3,40 @@ function I18n() {
 		'en': {
 			'settings.label.speed': 'Speed',
 			'settings.label.trace': 'Trace',
-			'settings.label.color': 'Color'
+			'settings.label.color': 'Color',
+
+			'tutorial.button.skip': 'Skip tutorial',
+			'tutorial.button.next': 'Next &gt;',
+			'tutorial.button.end': 'OK',
+			'tutorial.welcome.title': 'Welcome to Game of Grid!',
+			'tutorial.welcome.text': 'This game is freely inspired from the Conway\'s game of life<br/><br/>To play draw something on the grid and click on play',
+			'tutorial.play': 'Click play or hit the space bar to bring the grid to life',
+			'tutorial.history': 'Click undo/redo to cancel your last actions (Ctrl+Z/Ctrl+Y)',
+			'tutorial.clear': 'Click the recycle bin to clear the grid',
+			'tutorial.menu': 'Click here to toggle the panel',
+			'tutorial.sliders': 'Tweak speed, trace and color here',
+			'tutorial.symmetry': 'Choose the symmetry to draw with',
+			'tutorial.patterns': 'Click on these to draw a preset pattern',
+			'tutorial.fullscreen': 'Go into fullscreen'
 		},
 		'fr': {
 			'settings.label.speed': 'Vitesse',
 			'settings.label.trace': 'Trace',
-			'settings.label.color': 'Couleur'
+			'settings.label.color': 'Couleur',
+
+			'tutorial.button.skip': 'Skip tutorial',
+			'tutorial.button.next': 'Next &gt;',
+			'tutorial.button.end': 'OK',
+			'tutorial.welcome.title': 'Welcome to Game of Grid!',
+			'tutorial.welcome.text': 'This game is freely inspired from the Conway\'s game of life<br/><br/>To play draw something on the grid and click on play',
+			'tutorial.play': 'Click play or hit the space bar to bring the grid to life',
+			'tutorial.history': 'Click undo/redo to cancel your last actions (Ctrl+Z/Ctrl+Y)',
+			'tutorial.clear': 'Click the recycle bin to clear the grid',
+			'tutorial.menu': 'Click here to toggle the panel',
+			'tutorial.sliders': 'Tweak speed, trace and color here',
+			'tutorial.symmetry': 'Choose the symmetry to draw with',
+			'tutorial.patterns': 'Click on these to draw a preset pattern',
+			'tutorial.fullscreen': 'Go into fullscreen'
 		}
 	};
 	this.language = this.getBestLanguage();
---
+++ js/src/tutorial.js
@@ -0,0 +1,48 @@
+function Tutorial() {
+	this.popupIds = [
+		"gog-welcome-popup",
+		"gog-play-popup",
+		"gog-history-popup",
+		"gog-clear-popup",
+		"gog-menu-popup",
+		"gog-sliders-popup",
+		"gog-symmetry-popup",
+		"gog-patterns-popup",
+		"gog-fullscreen-popup"
+	];
+	
+	this.currentPopup = -1;
+}
+
+Tutorial.prototype.start = function() {
+	this.currentPopup = -1;
+	this.nextStep();
+}
+
+Tutorial.prototype.nextStep = function() {
+	if (this.currentPopup >= 0) {
+		this.hideCurrentPopup();
+	}
+
+	this.currentPopup++;
+
+	if (this.currentPopup < this.popupIds.length) {
+		var popup = document.getElementById(this.popupIds[this.currentPopup]);
+		popup.style.display = 'block';
+
+		var skipButtons = popup.getElementsByClassName("gog-skip-button");
+		if (skipButtons && skipButtons.length == 1) {
+			skipButtons[0].addEventListener("click", this.hideCurrentPopup.bind(this));
+		}
+
+		var nextButtons = popup.getElementsByClassName("gog-next-button");
+		if (nextButtons && nextButtons.length == 1) {
+			nextButtons[0].addEventListener("click", this.nextStep.bind(this));
+		}
+	}
+}
+
+Tutorial.prototype.hideCurrentPopup = function() {
+	var popup = document.getElementById(this.popupIds[this.currentPopup]);
+	popup.style.display = 'none';
+}
