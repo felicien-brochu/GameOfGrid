271450ccf89493682c366a785eaa49786b8aef8b
Félicien Brochu <felicien.brochu@gmail.com>
2016-11-08 14:22:01 +0100

    Change directories tree to src/build. New grunt tasks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,45 +2,80 @@
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
+		clean: {
+			all: 'build',
+			html: 'build/*.html',
+			css: 'build/css',
+			js: 'build/js',
+			svg: 'build/svg',
+			static: ['build/**/*', '!build/js/*', '!build/svg/*', 'build/index.html', 'build/css/*'],
+		},
+
+		copy: {
+			html: {
+				cwd: 'src',
+				src: ['*.html'],
+				dest: 'build',
+				expand: true,
+			},
+			css: {
+				cwd: 'src',
+				src: ['css/*.css'],
+				dest: 'build',
+				expand: true,
+			},
+			static: {
+				cwd: 'src',
+				src: ['**/*', '!*.html', '!css/**', '!js/**', '!svg/**', '!**/*.db'],
+				dest: 'build',
+				expand: true,
+			},
+		},
+
 		concat: {
 			options: {
-				sourceMap: true
+				sourceMap: true,
 			},
 			dist: {
-				src: 'js/src/*.js',
-				dest: 'js/build/gameofgrid-<%= pkg.version %>.js'
-			}
+				src: 'src/js/*.js',
+				dest: 'build/js/gameofgrid-<%= pkg.version %>.js',
+			},
 		},
 
 		uglify: {
 			options: {
 				mangle: false,
 				sourceMap: true,
-				sourceMapIn: 'js/build/gameofgrid-<%= pkg.version %>.js.map'
+				sourceMapIn: 'build/js/gameofgrid-<%= pkg.version %>.js.map',
 			},
 			build: {
-				src: 'js/build/gameofgrid-<%= pkg.version %>.js',
-				dest: 'js/build/gameofgrid-<%= pkg.version %>.min.js'
-			}
+				src: 'build/js/gameofgrid-<%= pkg.version %>.js',
+				dest: 'build/js/gameofgrid-<%= pkg.version %>.min.js',
+			},
 		},
-		
+
 		svgstore: {
 			options: {
 				prefix: 'shape-',
 			},
 			default: {
 				files: {
-					'svg/sprite.svg': ['svg/src/*.svg'],
-				}
-			}
+					'build/svg/sprite.svg': ['src/svg/*.svg'],
+				},
+			},
 		},
 	});
 
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-svgstore');
 
+	grunt.registerTask('html', ['copy:html']);
+	grunt.registerTask('css', ['clean:css', 'copy:css']);
+	grunt.registerTask('static', ['clean:static', 'copy:static']);
 	grunt.registerTask('js', ['concat', 'uglify']);
-	grunt.registerTask('graphics', ['svgstore']);
-	grunt.registerTask('default', ['js', 'graphics']);
+	grunt.registerTask('dev', ['js', 'html', 'css']);
+	grunt.registerTask('default', ['clean:all', 'html', 'copy:css', 'copy:static', 'js', 'svgstore']);
 };
